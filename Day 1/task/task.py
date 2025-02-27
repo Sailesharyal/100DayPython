fafea9848a6135db86620a9a1148cbdf9dafd27d
Sailesh Aryal <sailesharyal500@gmail.com>
2025-02-27 22:26:29 +0545

    complete a day 1 with auto name suggestion App

--- Day 1/task/task.py
+++ Day 1/task/task.py
@@ -1,3 +1,7 @@
-name = "Angela"
-length = len(name)
-print(length)
+
+print("Welcome to The auto Name suggestions for you band name")
+
+CityName = input("What is the name of City you grow up in ?\n")
+PetName = input("What is the name of a pet store?\n")
+
+print("Your band name could be "+ CityName +" "+ PetName + ".")
